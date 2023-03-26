27be33bd7494a1daba2bd9380abf6206a85db587
Ayo <alawani@students.kennesaw.edu>
2023-03-26 15:13:41 -0400

    Login form - username, password and submit

--- my_app/lib/sign_in_page.dart
+++ my_app/lib/sign_in_page.dart
@@ -1,4 +1,5 @@
 import 'package:english_words/english_words.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'main.dart';
@@ -11,6 +12,14 @@ class SignInPage extends StatefulWidget {
 }
 
 class _MySignInPage extends State<SignInPage> {
+  void _login() {
+    setState(() {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const MyAdventureQuestSignIn()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +30,42 @@ class _MySignInPage extends State<SignInPage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
+          children: <Widget>[
+            const Text(
               'Sign In Page',
               textAlign: TextAlign.start,
+            ),
+            const Text('\n'),
+            const SizedBox(
+              width: 300.0,
+              child: TextField(
+                maxLength: 40,
+                obscureText: false,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 300.0,
+              child: TextField(
+                maxLength: 40,
+                obscureText: false,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            const Text('\n'),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(300, 60),
+                maximumSize: const Size(300, 60),
+              ),
+              onPressed: _login,
+              child: const Text('Login'),
             ),
           ],
         ),
