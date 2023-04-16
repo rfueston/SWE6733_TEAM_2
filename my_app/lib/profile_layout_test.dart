6a4491ae028c79e5e803d1a1e9c92479387f3d7f
Ayo <124012258+Ayo-SWE@users.noreply.github.com>
2023-04-16 19:16:32 -0400

    Fixed scrolling

--- my_app/lib/profile_layout_test.dart
+++ my_app/lib/profile_layout_test.dart
@@ -73,110 +73,104 @@ class _MyProfileLayoutTest extends State<ProfileLayoutTest> {
     } */
   }
 
-  late final bioWidget = SingleChildScrollView(
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-        const Text(
-          'Bio Section',
-          textAlign: TextAlign.start,
+  late final bioWidget =
+      Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+    const Text(
+      'Bio Section',
+      textAlign: TextAlign.start,
+    ),
+    const Text('\n'),
+    const Image(
+      key: Key('profilePic'),
+      width: 300.0,
+      image: NetworkImage(
+          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+    ),
+    const Text('\n'),
+    SizedBox(
+      width: 600.0,
+      child: TextField(
+        key: Key('profileBio'),
+        //controller: myControllerBio,
+        maxLength: 1000,
+        obscureText: false,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'My Bio',
         ),
-        const Text('\n'),
-        const Image(
-          key: Key('profilePic'),
-          width: 300.0,
-          image: NetworkImage(
-              'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-        ),
-        const Text('\n'),
-        SizedBox(
-          width: 600.0,
-          child: TextField(
-            key: Key('profileBio'),
-            //controller: myControllerBio,
-            maxLength: 1000,
-            obscureText: false,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'My Bio',
-            ),
-          ),
-        ),
-      ]));
+      ),
+    ),
+  ]);
 
-  late final accountSettingsWidget = SingleChildScrollView(
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-        const Text(
-          'Account Settings',
-          textAlign: TextAlign.start,
+  late final accountSettingsWidget =
+      Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+    const Text(
+      'Account Settings',
+      textAlign: TextAlign.start,
+    ),
+    SizedBox(
+      width: 300.0,
+      child: TextField(
+        key: Key('profileUsername'),
+        enabled: false,
+        controller: myControllerDOB,
+        maxLength: 40,
+        obscureText: false,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Username',
         ),
-        SizedBox(
-          width: 300.0,
-          child: TextField(
-            key: Key('profileUsername'),
-            enabled: false,
-            controller: myControllerDOB,
-            maxLength: 40,
-            obscureText: false,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Username',
-            ),
-          ),
+      ),
+    ),
+    SizedBox(
+      width: 300.0,
+      child: TextField(
+        key: Key('profilePassword'),
+        enabled: false,
+        controller: myControllerEmail,
+        maxLength: 40,
+        obscureText: true,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Password',
         ),
-        SizedBox(
-          width: 300.0,
-          child: TextField(
-            key: Key('profilePassword'),
-            enabled: false,
-            controller: myControllerEmail,
-            maxLength: 40,
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Password',
-            ),
-          ),
+      ),
+    ),
+    SizedBox(
+      width: 300.0,
+      child: TextField(
+        key: Key('profileDOB'),
+        controller: myControllerDOB,
+        maxLength: 40,
+        obscureText: false,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'DOB',
         ),
-        SizedBox(
-          width: 300.0,
-          child: TextField(
-            key: Key('profileDOB'),
-            controller: myControllerDOB,
-            maxLength: 40,
-            obscureText: false,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'DOB',
-            ),
-          ),
+      ),
+    ),
+    SizedBox(
+      width: 300.0,
+      child: TextField(
+        key: Key('profileEmail'),
+        controller: myControllerEmail,
+        maxLength: 40,
+        obscureText: true,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Email',
         ),
-        SizedBox(
-          width: 300.0,
-          child: TextField(
-            key: Key('profileEmail'),
-            controller: myControllerEmail,
-            maxLength: 40,
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Email',
-            ),
-          ),
-        ),
-      ]));
+      ),
+    ),
+  ]);
 
-  late final locationSettingsWidget = SingleChildScrollView(
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-        const Text(
-          'Location Settings',
-          textAlign: TextAlign.start,
-        ),
-      ]));
+  late final locationSettingsWidget =
+      Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+    const Text(
+      'Location Settings',
+      textAlign: TextAlign.start,
+    ),
+  ]);
 
   late final activitiesSettingsWidget =
       Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
@@ -199,31 +193,17 @@ class _MyProfileLayoutTest extends State<ProfileLayoutTest> {
           children: <Widget>[
             bioWidget,
             const Text('\n'),
-            Container(
-              padding: const EdgeInsets.all(20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
+            //Container(
+            //  padding: const EdgeInsets.all(20),
+            //  child: Row(
+            //    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //    children: [
                   accountSettingsWidget,
                   locationSettingsWidget,
                   activitiesSettingsWidget,
-                ],
-              ),
-            ),
-
-/*             SizedBox(
-              width: 300.0,
-              child: TextField(
-                key: Key('profileDOB'),
-                controller: myControllerPassword,
-                maxLength: 40,
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-            ), */
+            //    ],
+            //  ),
+            //),
             ElevatedButton(
               key: Key('updateProfileButton'),
               style: ElevatedButton.styleFrom(
