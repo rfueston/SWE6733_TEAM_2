08e79de442ae0fb412ef644e27f0ada5b54e6b43
ScottDuong217 <98998893+ScottDuong217@users.noreply.github.com>
2023-04-19 19:56:38 -0500

    adventure_rating_page.dart test cases

---
+++ test/adventure_rating_page.dart
@@ -0,0 +1,64 @@
+import 'package:adventurequest/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+
+
+void main() async{
+
+  testWidgets('Adventure Rating Smoke Test: testAdventureRatingScreen', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const AdventureRatingPage());
+
+    // Verify that text starts on sign in page
+    expect(find.text('Activity: biking'), findsOneWidget);
+
+  });
+
+  testWidgets('Adventure Rating Smoke Test: testAdventureRatingScreen', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const AdventureRatingPage());
+
+    // Verify that text starts on sign in page
+    expect(find.text('Do you like biking?'), findsOneWidget);
+
+  });
+
+  testWidgets('Adventure Rating Smoke Test: testAdventureRatingScreen', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const AdventureRatingPage());
+
+    // Verify that text starts on sign in page
+    expect(find.text('Activity: hiking'), findsOneWidget);
+
+  });
+
+  testWidgets('Adventure Rating Smoke Test: testAdventureRatingScreen', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const AdventureRatingPage());
+
+    // Verify that text starts on sign in page
+    expect(find.text('Do you like hiking?'), findsOneWidget);
+
+  });
+
+  testWidgets('Adventure Rating Smoke Test: testAdventureRatingScreen', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const AdventureRatingPage());
+
+    // Verify that text starts on sign in page
+    expect(find.text('Activity: running'), findsOneWidget);
+
+  });
+
+  testWidgets('Adventure Rating Smoke Test: testAdventureRatingScreen', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const AdventureRatingPage());
+
+    // Verify that text starts on sign in page
+    expect(find.text('Do you like running?'), findsOneWidget);
+
+  });
+
+
